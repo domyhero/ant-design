9dde98118e11b28631951f6058fca374aa368916
Wei Zhu <yesmeck@gmail.com>
2017-11-10 17:25:24 +0800

    Mock rc-trigger for React 15

--- tests/__mocks__/rc-trigger.js
+++ tests/__mocks__/rc-trigger.js
@@ -1,3 +1,32 @@
-import Trigger from 'rc-trigger/lib/mock';
+import React from 'react';
+
+let Trigger; // eslint-disable-line
+
+if (process.env.REACT === '16') {
+  const TriggerMock = require('rc-trigger/lib/mock'); // eslint-disable-line
+  Trigger = TriggerMock;
+} else {
+  const ActualTrigger = require.requireActual('rc-trigger');
+  const render = ActualTrigger.prototype.render;
+
+  ActualTrigger.prototype.render = function () {
+    const { popupVisible } = this.state;
+    let component;
+
+    if (popupVisible || this._component) { // eslint-disable-line
+      component = this.getComponent();
+    }
+
+    return (
+      <div id="TriggerContainer">
+        {render.call(this)}
+        {component}
+      </div>
+    );
+  };
+
+  Trigger = ActualTrigger;
+}
+
 
 export default Trigger;
